ea48e9607b0063314f08ecd4aa9e9f536179aac0
taylortreece <tayloratreece@gmail.com>
2021-07-07 17:15:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// 1.
+//
+// correct:
+//
+// let main = document.getElementById('main')
+// main.remove()
+// 
+
+document.getElementById('main').remove()
+
+// 2.
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = 'is the champion'
